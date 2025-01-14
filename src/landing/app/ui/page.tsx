a96a21da29188a935126688a6967ba0f917d8b70
nikunj3011 <nikunjrathod3011@gmail.com>
2025-01-14 19:25:18 -0400

    updates on ui

--- src/landing/app/ui/page.tsx
+++ src/landing/app/ui/page.tsx
@@ -4,8 +4,9 @@ import { BorderBeam } from "@/components/ui/border-beam";
 import Image from 'next/image';
 import SparklesText from "@/components/ui/sparkles-text";
 import { NeonGradientCard } from "@/components/ui/neon-gradient-card";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+// import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import BlurFade from "@/components/ui/blur-fade";
+import { Card, Carousel } from "@/components/ui/apple-cards-carousel";
 const reviews = [
   {
     name: "Jack",
@@ -26,7 +27,119 @@ const reviews = [
     img: "https://avatar.vercel.sh/john",
   },
 ];
+const DummyContent = () => {
+  return (
+    <>
+      {[...new Array(3).fill(1)].map((_, index) => {
+        return (
+          <div
+            key={"dummy-content" + index}
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
+          >
+            <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+              <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                The first rule of Apple club is that you boast about Apple club.
+              </span>{" "}
+              Keep a journal, quickly jot down a grocery list, and take amazing
+              class notes. Want to convert those notes to text? No problem.
+              Langotiya jeetu ka mara hua yaar is ready to capture every
+              thought.
+            </p>
+            <Image
+              src="/NG_Logo_Clear.png"
+              alt="Macbook mockup from Aceternity UI"
+              height="500"
+              width="500"
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+const data2 = [
+  {
+    category: "Artificial Intelligence",
+    title: "You can do more with AI.",
+    src: "/NG_Logo_Clear.png",
+    content: <DummyContent />,
+  },
+  {
+    category: "Productivity",
+    title: "Enhance your productivity.",
+    src: "/NG_Logo_Clear.png",
+    content: <DummyContent />,
+  },
+  {
+    category: "Product",
+    title: "Launching the new Apple Vision Pro.",
+    src: "/NG_Logo_Clear.png",
+    content: <DummyContent />,
+  },
 
+  {
+    category: "Product",
+    title: "Maps for your iPhone 15 Pro Max.",
+    src: "/NG_Logo_Clear.png",
+    content: <DummyContent />,
+  },
+  {
+    category: "iOS",
+    title: "Photography just got better.",
+    src: "/NG_Logo_Clear.png",
+    content: <DummyContent />,
+  },
+  {
+    category: "Hiring",
+    title: "Hiring for a Staff Software Engineer",
+    src: "/NG_Logo_Clear.png",
+    content: <DummyContent />,
+  },
+];
+
+const cards = data2.map((card, index) => (
+    <Card key={card.src} card={card} index={index} />
+  ));
+  const data = [
+  {
+    category: "Artificial Intelligence",
+    title: "You can do more with AI.",
+    src: "/NG_Logo_Clear.png",
+    content: <DummyContent />,
+  },
+  {
+    category: "Productivity",
+    title: "Enhance your productivity.",
+    src: "/NG_Logo_Clear.png",
+    content: <DummyContent />,
+  },
+  {
+    category: "Product",
+    title: "Launching the new Apple Vision Pro.",
+    src: "/NG_Logo_Clear.png",
+    content: <DummyContent />,
+  },
+
+  {
+    category: "Product",
+    title: "Maps for your iPhone 15 Pro Max.",
+    src: "/NG_Logo_Clear.png",
+    content: <DummyContent />,
+  },
+  {
+    category: "iOS",
+    title: "Photography just got better.",
+    src: "/NG_Logo_Clear.png",
+    content: <DummyContent />,
+  },
+  {
+    category: "Hiring",
+    title: "Hiring for a Staff Software Engineer",
+    src: "/NG_Logo_Clear.png",
+    content: <DummyContent />,
+  },
+];
 const firstRow = reviews.slice(0, reviews.length / 2);
 const secondRow = reviews.slice(reviews.length / 2);
 
@@ -90,7 +203,16 @@ export default function MarqueeDemoVertical() {
           <SparklesText text="Define your style with us!" className="relative flex h-[50px] w-full flex-row text-3xl items-center justify-center overflow-hidden" />
         </div>
         </BlurFade><>
+        <div>
 
+        <h3>
+              <span className="w-full h-full py-20 pointer-events-none text-center text-xl font-semibold">
+              Your trusted partner in custom corporate apparel and promotional products. We specialize in providing high-quality, customizable clothing solutions to meet the unique needs of businesses across various industries. Whether you’re looking to outfit your team, promote your brand, or offer thoughtful gifts to clients, we’re here to help you make a lasting impression
+          </span>
+              </h3>
+        </div>
+        
+        <div>
         {/* <div className="relative flex h-[500px] w-full flex-row items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
         <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
           <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/90 bg-clip-text text-center text-4xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-00/10">
@@ -99,25 +221,26 @@ export default function MarqueeDemoVertical() {
           <BorderBeam size={500} duration={12} delay={9} />
         </div>
         </div> */}
-        <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border ">
-          <span className="pointer-events-none text-center text-xl font-semibold">
-            Define your style with us!
-          </span>
-          <BorderBeam size={250} duration={5} delay={9} />
+        <div className="relative flex  w-full flex-col items-center justify-center overflow-hidden rounded-lg border ">
+        <div className="w-full h-full py-20">
+              
+              
+              <Carousel items={cards} />
+          </div>
+          <BorderBeam size={550} duration={5} delay={9} />
         </div>
-
-        <div>
-          <Card className="w-[350px] border-0">
+        
+          {/* <Card className="w-[350px] border-0">
             <CardHeader>
               <CardTitle>Create project</CardTitle>
               <CardDescription>Deploy your new project in one-click.</CardDescription>
             </CardHeader>
             <CardContent>
               <span className="pointer-events-none text-center text-xl font-semibold">
-                Define your style with us!
+                Daa!
               </span>
             </CardContent>
-          </Card>
+          </Card> */}
         </div>
         {/* <div className="relative flex w-full flex-row items-center justify-center overflow-hidden">
     <NeonGradientCard className="max-w-sm items-center justify-center text-center">

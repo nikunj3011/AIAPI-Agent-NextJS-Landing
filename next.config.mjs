62eb106e17d844ecdc454e7f983f40d2546935f1
nikunj3011 <nikunjrathod3011@gmail.com>
2025-01-16 01:24:33 -0400

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/AIAPI-Agent-NextJS-Landing/src/landing",
+    basePath: "/AIAPI-Agent-NextJS-Landing",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
     images: { unoptimized: true }
